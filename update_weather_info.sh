9d6197b8826478f1d8a7473480170d904f7498bc
Marco Bakera <marco@bakera.de>
2018-06-24 13:16:02 +0200

    init.

---
+++ update_weather_info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PDFURL=https://www.yr.no/place/Germany/North_Rhine-Westphalia/Bochum/forecast.pdf
+OUTPDF=forecast.pdf
+OUTPNG=forecast.png
+OUTCROPPED=forecast-cropped.png
+DIMENSION=1501x600+130+400
+DPI=300
+
+wget -O $OUTPDF $PDFURL
+
+# convert to image
+convert -density $DPI $OUTPDF $OUTPNG
+
+# crop image
+convert -crop $DIMENSION +repage $OUTPNG $OUTCROPPED
