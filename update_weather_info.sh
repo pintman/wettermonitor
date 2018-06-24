ea6f12a64e88657a79f42ed70ee498059c45179f
Marco Bakera <marco@bakera.de>
2018-06-24 13:30:30 +0200

    changed dimensions.

--- update_weather_info.sh
+++ update_weather_info.sh
@@ -4,7 +4,7 @@ PDFURL=https://www.yr.no/place/Germany/North_Rhine-Westphalia/Bochum/forecast.pd
 OUTPDF=forecast.pdf
 OUTPNG=forecast.png
 OUTCROPPED=forecast-cropped.png
-DIMENSION=1501x600+130+400
+DIMENSION=1100x700+130+400
 DPI=300
 
 wget -O $OUTPDF $PDFURL
